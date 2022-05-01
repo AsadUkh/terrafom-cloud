c6243942e813b48cd309809c7932664dcdbf2e77
Asad ullah khan <asad.ullah.khan@macys.com>
2022-05-01 17:30:30 +0500

    rrmove buket

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "tf-cloud-tesst-bucket" {
-  bucket = "tf-cloud-tesst-bucket"
+# resource "aws_s3_bucket" "tf-cloud-tesst-bucket" {
+#   bucket = "tf-cloud-tesst-bucket"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }

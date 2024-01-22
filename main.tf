d3fbda11bb994e94bbb30bfcba7c211fc6f9cc0b
Asad Ullah Khan <asad.ullah.khan@macys.com>
2024-01-22 22:06:29 +0500

    i am adding onne more bucket after long time

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 resource "aws_s3_bucket" "tf-cloud-tesst-bucket" {
+  bucket = "tf-cloud-tesst-bucket-1"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "tf-cloud-tesst-bucket" {
   bucket = "tf-cloud-tesst-bucket"
 
   tags = {

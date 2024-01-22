60cb1047411340c60123fda6ee37fd94f8644543
Asad Ullah Khan <asad.ullah.khan@macys.com>
2024-01-23 02:30:55 +0500

    remove bucket

--- main.tf
+++ main.tf
@@ -7,11 +7,4 @@ resource "aws_s3_bucket" "tf-cloud-tesst-bucket-1" {
   }
 }
 
-resource "aws_s3_bucket" "tf-cloud-tesst-bucket" {
-  bucket = "tf-cloud-tesst-bucket"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
